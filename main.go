c4352189eced3a52c266ef798c3e8859a61ff0be
saisumith770 <saisumith812@gmail.com>
2022-09-17 01:32:53 +0530

    added access based control to the server

--- main.go
+++ main.go
@@ -3,22 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"com.mixin.morphine/core"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("SYSTEM::ENVIRONMENT: failed to load environment variables from godotenv")
+	}
 	hub := core.Generate_HubService()
 	router := mux.NewRouter()
 
 	go hub.Run()
+
+	router.HandleFunc("/analytics", func(res http.ResponseWriter, req *http.Request) {
+		access_token := strings.TrimPrefix(req.Header.Get("Access-Token"), "Bearer ")
+		if access_token == os.Getenv("ADMIN_ACCESS") {
+			//provide server analytics here
+		} else {
+			res.WriteHeader(401)
+		}
+	}).Methods("GET")
+
 	router.HandleFunc("/connect", func(res http.ResponseWriter, req *http.Request) {
 		access_token := strings.TrimPrefix(req.Header.Get("Access-Token"), "Bearer ")
-		log.Print(access_token)
-		//check whether the proper access token is provided
-		//access token must be stored with a salt
+		switch access_token {
+		case os.Getenv("ADMIN_ACCESS"):
+		case os.Getenv("SERVER_ACCESS"):
+		case os.Getenv("USER_ACCESS"):
+		default:
+			res.WriteHeader(401)
+			return
+		}
 		core.Generate_ClientWS(hub, req, res)
 	})
 
