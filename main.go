f808fcaaadfc4e66f852fba3a3a4c5dfef7c42ca
saisumith770 <saisumith812@gmail.com>
2022-11-12 10:48:21 +0530

    fixing my issue with git linux

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// work on implementations
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("SYSTEM::ENVIRONMENT: failed to load environment variables from godotenv")
